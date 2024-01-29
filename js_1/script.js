429e8b98f24bebd48baff9595221198947cad6d6
Robson <142177762+robsthecreator@users.noreply.github.com>
2024-01-29 17:37:13 -0300

    mais opções de mensagem

--- js_1/script.js
+++ js_1/script.js
@@ -1,3 +1,22 @@
-    function connect_button(){
-        return console.log('Conexão feita com sucesso!')
-    }
+function connect_button(){
+    return console.log('Conexão feita com sucesso!')
+}
+
+
+// opção sem o botão
+
+function connect(){
+    return console.log('Conexão feita com sucesso!')
+}
+
+// opção com pop-up ao abrir o site
+
+function connect_popup(){
+    window.alert('Conexão feita com sucesso!')
+}
+
+connect_popup()
+
+connect()
+connect()
+connect()
